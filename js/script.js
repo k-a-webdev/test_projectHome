0d3d0cff99c1038145a6ecf7d69a5f732427829c
k-a-webdev <kuzko.andrii.webdev@gmail.com>
2023-08-02 13:47:34 +0300

    Change some styles

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 const swiper = new Swiper(".swiper", {
     // Optional parameters
     direction: "horizontal",
-    loop: true,
+    loop: false,
 
     // If we need pagination
     pagination: {
@@ -28,4 +28,54 @@ const swiper = new Swiper(".swiper", {
         nextEl: ".btn_next",
         prevEl: ".btn_prev",
     },
+});
+
+// Active header
+const header = document.querySelector("header");
+const banner = document.querySelector("#home");
+// const toTop = document.querySelector(".to_top");
+const bannerY = banner.getBoundingClientRect().bottom;
+
+const about = document.querySelector("#about");
+const projects = document.querySelector("#projects");
+const contact = document.querySelector("#contact");
+const headerItems = header.querySelectorAll("li");
+
+const clearHeaderItems = () => {
+    headerItems.forEach((el) => {
+        el.classList.remove("active");
+    });
+};
+
+document.addEventListener("scroll", function () {
+    header.classList.toggle(
+        "active",
+        window.scrollY > header.getBoundingClientRect().top + 1
+    );
+    // window.scrollY / 2 > bannerY
+    //     ? (toTop.style.display = "flex")
+    //     : (toTop.style.display = "none");
+    const homeTop = banner.getBoundingClientRect().top;
+    const aboutTop = about.getBoundingClientRect().top;
+    const projectsTop = projects.getBoundingClientRect().top;
+    const contactTop = contact.getBoundingClientRect().top;
+
+    if (homeTop <= 0) {
+        clearHeaderItems();
+        headerItems[0].classList.add("active")
+    }
+    if (aboutTop <= 500 && (aboutTop + about.querySelector('h1').clientHeight <= window.innerHeight)) {
+        clearHeaderItems();
+        headerItems[1].classList.add("active");
+    }
+    if (projectsTop <= 500 && (projectsTop + projects.querySelector('h1').clientHeight <= window.innerHeight)) {
+        console.log(projectsTop)
+
+        clearHeaderItems();
+        headerItems[2].classList.add("active");
+    }
+    if (contactTop <= 600 && (contactTop + contact.querySelector('h1').clientHeight <= window.innerHeight)) {
+        clearHeaderItems();
+        headerItems[4].classList.add("active");
+    }
 });
